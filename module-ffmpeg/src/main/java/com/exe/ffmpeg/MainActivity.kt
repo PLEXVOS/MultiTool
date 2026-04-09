ef4cda63a7399c02baab3759bd24cadd751981f9
PLEXVOS <usa.plexvos@gmail.com>
2026-04-09 11:04:58 +0200

    Update MainActivity.kt

--- module-ffmpeg/src/main/java/com/exe/ffmpeg/MainActivity.kt
+++ module-ffmpeg/src/main/java/com/exe/ffmpeg/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ffmpeg_activity_main)
 
         val rv = findViewById<RecyclerView>(R.id.recyclerView)
         rv.layoutManager = LinearLayoutManager(this)
